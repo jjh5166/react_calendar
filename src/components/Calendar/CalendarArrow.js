e1b95a1221befcf58674c4e6c657ffba74b124f4
John <jjh5166@gmail.com>
2019-11-19 01:12:56 -0500

    rm header from table, styled components

--- src/components/Calendar/CalendarArrow.js
+++ src/components/Calendar/CalendarArrow.js
@@ -1,25 +1,28 @@
 import React from "react";
+import styled from 'styled-components';
 import { withMomenter } from '../Momenter';
 
 function CalendarArrow(props) {
   switch (props.state) {
     case 'left':
       return (
-        < td className="nav-month" >
+        <MonthArrow>
           <i className="fa fa-chevron-left" aria-hidden="true"
-            onClick={(e) => {props.momenter.prevMonth()}}></i>
-        </td >
+            onClick={(e) => { props.momenter.prevMonth() }}></i>
+        </MonthArrow >
       )
     case 'right':
       return (
-        <td className="nav-month">
+        <MonthArrow>
           <i className="fa fa-chevron-right" aria-hidden="true"
-            onClick={(e) =>  props.momenter.nextMonth()}></i>
-        </td>
+            onClick={(e) => props.momenter.nextMonth()}></i>
+        </MonthArrow>
       )
     default:
       return null
   }
 }
-
+const MonthArrow = styled.div`
+  ${props => props.state == 'left' ? `margin-left: 1%` : `margin-right: 1%`};
+`
 export default withMomenter(CalendarArrow);
