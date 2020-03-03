1a10d024d395adba57854488f336aeba9a42d321
John <jjh5166@gmail.com>
2020-03-02 21:08:01 -0500

    Div100vh

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
+import Div100vh from 'react-div-100vh';
 import styled from 'styled-components';
 import Calendar from '../Calendar'
 
 class App extends Component {
   render() {
     return (
-      <AppDiv>
+      <Div100vh>
         <Calendar />
-      </AppDiv>
+      </Div100vh>
     )
-    }
+  }
 }
 
-const AppDiv = styled.div`
-  height:100%;
-  width:100%;
-  `
 export default App;
