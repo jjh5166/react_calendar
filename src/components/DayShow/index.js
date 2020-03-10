68451ea952a106b8d71e9fb16faef8a5e987a00c
John <jjh5166@gmail.com>
2020-03-10 19:06:45 -0400

    dayshow init

---
+++ src/components/DayShow/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { DayShowContainer } from './styled';
+
+function DayShow(props) {
+  return(
+    <DayShowContainer>
+
+    </DayShowContainer>
+  )
+}
+
+export default DayShow;
---
+++ src/components/DayShow/styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const DayShowContainer = styled.div`
+  outline: solid red 2px;
+`
