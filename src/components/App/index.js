2525ebac521a592a3ab19e0c50969844370c9d3a
John <jjh5166@gmail.com>
2020-03-13 17:09:40 -0400

    GeoContext

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import Div100vh from 'react-div-100vh';
+import { geolocated } from 'react-geolocated';
+
 import Calendar from '../Calendar'
+import { GeoContext } from '../GeoLoc'
 
 class App extends Component {
   render() {
     return (
-      <Div100vh>
-        <Calendar />
-      </Div100vh>
+      <GeoContext.Provider value={this.props.coords}>
+        <Div100vh>
+          <Calendar />
+        </Div100vh>
+      </GeoContext.Provider>
     )
   }
 }
+const AppWithGeoloc = geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(App);
 
-export default App;
+export default AppWithGeoloc;
