bb10ef5f75351090f9dcd727605bcc916ce8209f
John <jjh5166@gmail.com>
2020-03-12 22:04:38 -0400

    DayShow with date

--- src/components/DayShow/styled.js
+++ src/components/DayShow/styled.js
@@ -3,6 +3,9 @@ import { device } from '../../constants';
 
 export const DayShowContainer = styled.div`
   position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 100%;
   width: 100%;
   flex: 1;
@@ -13,16 +16,13 @@ export const DayShowContainer = styled.div`
     width: 40%;
   }
 `
-export const ShowDate = styled.div`
-  width: 100%;
-  text-align: center;
-`
 export const EventList = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  min-height: 80%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80%;
   width: 80%;
   @media ${device.laptop} {
     border-radius: 30px;
@@ -33,6 +33,21 @@ export const EventList = styled.div`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    content: '${props => props.date}'
+    color: #abacb0;
+    font-size: 240px;
+    z-index: -1;
+    content: '${props => props.date}';
+    transition: 0.5s all ease-out;
+    @media ${device.landscapeMobile} {
+      font-size: 300px;
+    }
+    @media ${device.laptop} {
+      font-size: 350px;
+    }
   }
+`
+export const EventItem = styled.div`
+  width: 80%;
+  border-bottom: solid black 1px;
+  min-height: 35px;
 `
