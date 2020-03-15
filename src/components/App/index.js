d6d31b4c42c73549ec497047911ad6dae9d0b594
John <jjh5166@gmail.com>
2020-03-14 23:50:37 -0400

    stateless App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Div100vh from 'react-div-100vh';
 import { geolocated } from 'react-geolocated';
 
 import Calendar from '../Calendar'
 import { GeoContext } from '../GeoLoc'
 
-class App extends Component {
-  render() {
-    return (
-      <GeoContext.Provider value={this.props.coords}>
-        <Div100vh>
-          <Calendar />
-        </Div100vh>
-      </GeoContext.Provider>
-    )
-  }
+const App = (props) => {
+  return (
+    <GeoContext.Provider value={props.coords}>
+      <Div100vh>
+        <Calendar />
+      </Div100vh>
+    </GeoContext.Provider>
+  )
 }
 const AppWithGeoloc = geolocated({
   positionOptions: {
