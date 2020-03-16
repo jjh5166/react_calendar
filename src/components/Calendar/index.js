56d7537f403b8174d0df60ae61e2c61ee06de0cf
John <jjh5166@gmail.com>
2020-03-16 00:02:21 -0400

    rm axios, connect redux

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,63 +1,28 @@
 import React from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 import moment from 'moment';
 
 import Momenter, { MomenterContext } from '../Momenter';
 import { withGeo } from '../GeoLoc'
+import { fetchWeather } from '../../actions/weather';
+
 import CalendarBody from './CalendarBody';
 import CalendarHeader from './CalendarHeader';
 import DayShow from '../DayShow';
 import { CalendarContainer, OptionalWrapper } from './styled';
-
-const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-const PATH_BASE = 'https://api.openweathermap.org/data/2.5/forecast';
-const DEFAULT_COORDS = {
-  lat: 51.5073,
-  lon: -0.1277
-}
-const TEMP_TYPE = 'imperial'
 
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       dateContext: moment(),
-      weather: null,
-      coords: DEFAULT_COORDS,
-      city: null,
-      error: null
     };
   }
   updateDateContext = (dateContext) => {
     this.setState({ dateContext })
   }
-  handleWeatherResult(result) {
-    this.setState({
-      city: result['data']['city'],
-      weather: this.parseWeather(result['data']['list'])
-    });
-  }
-  parseWeather(list) {
-    const dateEx = /\d{1,2}(?=[ ])/
-    const noonEx = /12:/
-    let parsedWeather = [list[0]]
-    let gotWeather = []
-    for (let i = 0; i < list.length; i++) {
-      let time = list[i]["dt_txt"];
-      if (i === 0) {
-        gotWeather.push(time.match(dateEx)[0])
-      }
-      if (noonEx.test(time) && !gotWeather.includes(time.match(dateEx)[0])) {
-        parsedWeather.push(list[i])
-      }
-    }
-    return parsedWeather
-  }
   componentDidMount() {
-    const { coords } = this.state;
-    axios.get(`${PATH_BASE}?lat=${coords.lat}&lon=${coords.lon}&units=${TEMP_TYPE}&appid=${WEATHER_API_KEY}`)
-      .then(result => { this.handleWeatherResult(result) })
-      .catch((error) => { this.setState({ error }) })
+    this.props.dispatch(fetchWeather());
   }
   render() {
     return (
@@ -74,4 +39,11 @@ class Calendar extends React.Component {
   }
 }
 
-export default withGeo(Calendar);
+const mapStateToProps = (state) => {
+
+  return {
+
+  };
+}
+
+export default connect(mapStateToProps)(withGeo(Calendar));
