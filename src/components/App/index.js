9c395c29c71bc6b4765fce66052cf35d36acbbf8
John <jjh5166@gmail.com>
2020-03-18 00:32:02 -0400

    rm geolocated

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Div100vh from 'react-div-100vh';
-import { geolocated } from 'react-geolocated';
 
 import Calendar from '../Calendar'
-import { GeoContext } from '../GeoLoc'
 import store from '../../config/store';
 
-const App = (props) => {
-  return (
-    <Provider store={store} >
-      <GeoContext.Provider value={props.coords}>
-        <Div100vh>
-          <Calendar />
-        </Div100vh>
-      </GeoContext.Provider>
-    </Provider>
-  )
-}
-const AppWithGeoloc = geolocated({
-  positionOptions: {
-    enableHighAccuracy: false,
-  },
-  userDecisionTimeout: 5000,
-})(App);
-
-export default AppWithGeoloc;
+export default () => (
+  <Provider store={store} >
+    <Div100vh>
+      <Calendar />
+    </Div100vh>
+  </Provider>
+)
