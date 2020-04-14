2056503a33c5e791c1c260d1340809a0bdb630b4
John <jjh5166@gmail.com>
2020-04-14 11:33:22 -0400

    otd refactor

--- src/components/OnThisDay/index.js
+++ src/components/OnThisDay/index.js
@@ -1,53 +1,58 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Loader from 'react-loader-spinner';
 import ReactHtmlParser from 'react-html-parser';
 
 import { getOnThisDay } from '../../actions/onThisDay';
 import { withMomenter } from '../Momenter';
-import { OtdContainer, OtdContent } from './styled';
+import { OtdContainer, OtdContentContainer, OTDHeader } from './styled';
 
 const choseEvent = (events) => {
   let rando = Math.floor(Math.random() * 5);
 
-  return events[rando].html
+  return events[rando]
 }
 
 const OnThisDay = (props) => {
   const { onThisData, momenter, dispatch, theme } = props;
-  let activated = false
   let chosen = null;
+
   useEffect(() => {
     if (!onThisData[momenter.otdDate]) {
       dispatch(getOnThisDay(momenter.otdDate))
     }
   });
   if (onThisData.hasOwnProperty(momenter.otdDate)) {
-    activated = true
     if (onThisData[momenter.otdDate].events.length !== 0) {
       chosen = choseEvent(onThisData[momenter.otdDate].events)
     }
+    return (
+      <OtdContainer>
+        <Loader
+          type="Grid"
+          height={100}
+          width={100}
+          color={theme.fifthColor}
+          visible={onThisData[momenter.otdDate].isFetching}
+        />
+        <OtdContent content={chosen} />
+      </OtdContainer>
+    )
+  }
+  return null;
+}
+
+const OtdContent = ({ content }) => {
+  if (content) {
+    return (
+      <OtdContentContainer>
+        <OTDHeader>{`On This Day in ${content.year}`}</OTDHeader>
+        <p>{ReactHtmlParser(content.no_year_html)}</p>
+      </OtdContentContainer>
+    )
   }
 
-  return (
-    <OtdContainer>
-      {
-        (activated) &&
-        <Fragment>
-          <Loader
-            type="Grid"
-            height={100}
-            width={100}
-            color={theme.fifthColor}
-            visible={onThisData[momenter.otdDate].isFetching}
-          />
-          <OtdContent>
-            <p>{ReactHtmlParser(chosen)}</p>
-          </OtdContent>
-        </Fragment>
-      }
-    </OtdContainer>
-  )
+  return null;
 }
 
 const mapStateToProps = (state) => {
--- src/components/OnThisDay/styled.js
+++ src/components/OnThisDay/styled.js
@@ -4,7 +4,7 @@ import { transparentize } from 'polished';
 export const OtdContainer = styled.div`
   z-index: 1;
 `
-export const OtdContent = styled.div`
+export const OtdContentContainer = styled.div`
   border-radius: 30px;
   background: ${props => transparentize(0.2, props.theme.mainColor)};
   border: solid ${props => props.theme.fifthColor} 2px;
@@ -12,4 +12,9 @@ export const OtdContent = styled.div`
   p{
     margin: 14px;
   }
+`
+export const OTDHeader = styled.h4`
+  text-align: center;
+  text-decoration: underline;
+  margin: 9px;
 `
