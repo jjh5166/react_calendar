5713225f733fc72ff06e62a9e1a5518f7c1652bd
John <jjh5166@gmail.com>
2021-03-02 16:00:05 +0000

    use axios for weather

--- src/config/sagas.js
+++ src/config/sagas.js
@@ -9,18 +9,20 @@ const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const PATH_BASE = 'https://api.openweathermap.org/data/2.5/forecast';
 const TEMP_TYPE = 'imperial';
 
-const getWeather = (coords) => fetch(`${PATH_BASE}?lat=${coords.lat}&lon=${coords.lon}&units=${TEMP_TYPE}&appid=${WEATHER_API_KEY}`)
+const getWeather = (coords) =>
+  axios(
+    `${PATH_BASE}?lat=${coords.lat}&lon=${coords.lon}&units=${TEMP_TYPE}&appid=${WEATHER_API_KEY}`
+  )
 const getOnThisDay = (date) => axios.get(`${process.env.REACT_APP_CORS_PROXY}http://history.muffinlabs.com/date/${date}`) //http://history.muffinlabs.com/date/2/14
 const getLocale = (coords) => axios.get(`https://us1.locationiq.com/v1/reverse.php?key=${process.env.REACT_APP_LOCATION_KEY}&lat=${coords.lat}&lon=${coords.lon}&format=json`)
 
 function* fetchWeather(action) {
   try {
     const response = yield call(getWeather,action.coords);
-    const result = yield response.json();
-    if (result.error) {
-      yield put({ type: WEATHER_ERROR, error: result.error })
+    if (response.error) {
+      yield put({ type: WEATHER_ERROR, error: response.error })
     } else {
-      yield put({ type: WEATHER_RESULT, result })
+      yield put({ type: WEATHER_RESULT, result: response.data })
     }
   } catch (e) {
     yield put({ type: WEATHER_ERROR, error: e.message })
